2a3d2b80d2cc68ea8212289369a2341caa07ed00
easeghehey <emanuelaseghey9@gmail.com>
2022-08-04 16:26:33 -0400

    pdf link

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -3,8 +3,8 @@ import CV from '../../assets/EA_resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Resume</a>
-        {/* <a href="https://docs.google.com/document/d/1lMnt7Oo5tOr4AC2CuU2aH6N4CvqjWzanhc6hTJ1Zcqs/edit" target="__blank" rel="noreferrer" className='btn'>Resume</a> */}
+        {/* <a href={CV} download className='btn'>Resume</a> */}
+        <a href="https://docdro.id/abnslJb" target="__blank" rel="noreferrer" className='btn'>Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact Me!</a>
     </div>
   )
